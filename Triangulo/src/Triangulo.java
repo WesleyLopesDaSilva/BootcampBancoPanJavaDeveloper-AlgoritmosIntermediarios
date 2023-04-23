d07e2619b648783564bcae55ca7408d4d920fcd1
Wesley Lopes <wesley.lopes0902@gmail.com>
2023-04-23 19:09:20 -0300

    Versão Final

--- Triangulo/src/Triangulo.java
+++ Triangulo/src/Triangulo.java
@@ -1,5 +1,51 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Triangulo {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite o valor de A: ");
+		double A = leitor.nextDouble();
+        System.out.println("Digite o valor de B: ");
+		double B = leitor.nextDouble();
+        System.out.println("Digite o valor de C: ");
+		double C = leitor.nextDouble();
+		double maior;
+		double soma;
+		if(A + B == C || A + C == B || B + C == A){
+		  maior = ((A + B)* C)/2;
+		  System.out.println("Area = " + maior);
+		} else{
+		  soma = A + B + C;
+		  System.out.println("Perimetro = " + soma);
+		}
     }
 }
+
+/*
+
+--> Exemplo de Código aceito pela DIO
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Teste {
+
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        double A = leitor.nextDouble();
+        double B = leitor.nextDouble();
+        double C = leitor.nextDouble();
+        double maior;
+        double soma;
+        boolean triangulo;
+        if (A + B == C || A + C == B || B + C == A) {
+            maior = ((A + B) * C) / 2;
+            System.out.println("Area = " + maior);
+        } else {
+            soma = A + B + C;
+            System.out.println("Perimetro = " + soma);
+        }
+    }
+}
+
+*/
